23fe3b48c026154490e824fc1f49c542c3ddf2c5
ikaro123-hash <ikaro.praxedes@escolar.ifrn.edu.br>
2025-11-29 12:07:39 -0300

    Primeiro commit: adicionando todos os arquivos do projeto

--- django_backend/core/auth_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import auth_views
-
-urlpatterns = [
-    path('login/', auth_views.custom_login, name='login'),
-    path('logout/', auth_views.custom_logout, name='logout'),
-    path('register/', auth_views.CustomRegisterView.as_view(), name='register'),
-    path('profile/', auth_views.profile, name='profile'),
-]
