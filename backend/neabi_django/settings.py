8f59fc495368cc79b67bdf53317e1cf57d7dd9a0
ikaro123-hash <ikaro.praxedes@escolar.ifrn.edu.br>
2025-11-30 09:04:34 -0300

    Update EMAIL_HOST_PASSWORD environment variable

--- backend/neabi_django/settings.py
+++ backend/neabi_django/settings.py
@@ -132,9 +132,8 @@ EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("eoxr kfwh amuj hhfs")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 
 # Security settings for production
 if not DEBUG:
