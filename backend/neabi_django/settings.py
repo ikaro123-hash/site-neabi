85fec724161aae4dd2dd1006ac331c6ddbfaa361
ikaro123-hash <ikaro.praxedes@escolar.ifrn.edu.br>
2025-12-01 21:06:36 -0300

    Switch database configuration to use dj_database_url

--- backend/neabi_django/settings.py
+++ backend/neabi_django/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,11 +63,13 @@ WSGI_APPLICATION = 'neabi_django.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
